ac958609d220db05b2175804d6d531a8cf3ab17d
jifakir <jifakir@outlook.com>
2021-09-26 17:29:11 +0600

    Updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '@/plugins/vue-toastify.js', mode: 'client' },
-    { src: '~/plugins/vuex-persist', ssr: false }
+    { src: '~/plugins/vuex-persist', ssr: false } 
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
